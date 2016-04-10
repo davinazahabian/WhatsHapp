a67d11cba94465d40549271c5e4c402812ce8870
nikhilcherukuri <nikhilcherukuri@10.123.101.18>
2016-04-10 16:04:56 -0700

    new user

--- WHFrame/NewUserGUI.java
+++ WHFrame/NewUserGUI.java
@@ -1,11 +1,14 @@
 package WHFrame;
 
 
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,6 +17,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+import library.ImageLibrary;
 
 
 
@@ -30,7 +35,7 @@ public class NewUserGUI extends JFrame{
 	 JLabel firstNameLabel;
 	 JLabel lastNameLabel;
 	 JLabel emailLabel;
-	 
+	 SplashPanel splash;
 	 JButton backButton;
 	 JButton signUpButton;
 	 
@@ -72,43 +77,44 @@ public class NewUserGUI extends JFrame{
 		ImageIcon water = new ImageIcon("back-icon.png");
 	    backButton = new JButton(water);
 	   
-	    
+	    splash = new SplashPanel();
 	}
 	
 	private void create() {
-		setLayout(new GridBagLayout());
+		setLayout(new GridLayout(1, 1));
+		splash.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.insets = new Insets(3,3,3,3);
-		add(usernameLabel,c);
-		add(usernameField,c);
+		splash.add(usernameLabel,c);
+		splash.add(usernameField,c);
 		c.gridy = 2;
-		add(passwordLabel,c);
-		add(passwordField,c);
+		splash.add(passwordLabel,c);
+		splash.add(passwordField,c);
 		
 		c.gridy = 3;
-		add(firstNameLabel,c);
-		add(firstNameField,c);
+		splash.add(firstNameLabel,c);
+		splash.add(firstNameField,c);
 		
 		c.gridy = 4;
-		add(lastNameLabel,c);
-		add(lastNameField,c);
+		splash.add(lastNameLabel,c);
+		splash.add(lastNameField,c);
 		
 		c.gridy = 5;
-		add(emailLabel,c);
-		add(emailField,c);
+		splash.add(emailLabel,c);
+		splash.add(emailField,c);
 		
 		c.gridy = 6;
 		c.gridwidth = 6;
-		add(signUpButton,c);
+		splash.add(signUpButton,c);
 //		c.anchor = GridBagConstraints.FIRST_LINE_START;
 //		
 //		JPanel panel = new JPanel();
 //		panel.setLayout(new GridLayout(1, 1));
 //		panel.setSize(new Dimension(50, 50));
-//		panel.add(backButton);
+//		panel.splash.add(backButton);
 //		panel.setSize(new Dimension(50, 50));
-//		add(panel);
-		
+//		splash.add(panel);
+		add(splash);
 	}
 
 	private void addActions() {
@@ -117,6 +123,10 @@ public class NewUserGUI extends JFrame{
 	}
 	
 	public static void main(String[] args){
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Cursor c = toolkit.createCustomCursor(ImageLibrary.getImage("img/cursor.png") , new Point(0, 0), "img");
+	
+		//whf.setCursor(c);
 		new NewUserGUI();
 	}
 
