32417cb386a738002afb1b705ce1c0ea22262cab
Justine Nour-Omid <jnouromi@usc.edu>
2016-04-12 18:27:59 -0700

    eventdetailGUI improved

--- client/EventDetailGUI.java
+++ client/EventDetailGUI.java
@@ -1,134 +1,183 @@
 package client;
-//import java.awt.BorderLayout;
-//import java.awt.Color;
-//import java.awt.Dimension;
-//import java.awt.Font;
-//import java.awt.GridBagConstraints;
-//import java.awt.GridBagLayout;
-//import java.util.Vector;
-//
-//import javax.swing.Box;
-//import javax.swing.BoxLayout;
-//import javax.swing.JButton;
-//import javax.swing.JFrame;
-//import javax.swing.JLabel;
-//import javax.swing.JPanel;
-//import javax.swing.JTextArea;
-//
-//import customui.WHButton;
-//
-//public class EventDetailGUI extends JPanel {
-//	
-//	private static final long serialVersionUID = 1L;
-//	private Event e;
-//	private Color redColor;
-//	private Font font;
-//	private JPanel mainPanel;
-//	
-//	private JButton backButton;
-//	private JLabel eventTitle;
-//	private JPanel eventInfoPanel;
-//	private JLabel eventDate;
-//	private JLabel eventTime;
-//	private JLabel eventLocation;
-//	private JLabel eventDetail;
-//	
-//	private JPanel messageBoardPanel;
-//	private JLabel messageBoardLabel;
-//	private JTextArea messageBoard;
-//	private JPanel addPanel;
-//	private JTextArea addMessageArea;
-//	private WHButton sendMessage;
-//	
-//	public EventDetailGUI(Event e) {
-//		super(new GridBagLayout());
-//		this.setSize(new Dimension(900,602));
-//		this.e = e;
-//		
-//		instantiateComponents();
-//		createGUI();
-//		addActions();
-//	}
-//	
-//	private void instantiateComponents() {
-//		redColor = new Color(130,25,44);
-//		mainPanel = new JPanel();
-//		font = new Font("Arial", Font.BOLD, 26);
-//		// this should be an arrow image:
-//		backButton = new JButton();
-//		eventInfoPanel = new JPanel();
-//		eventInfoPanel.setLayout(new BoxLayout(eventInfoPanel, BoxLayout.Y_AXIS));
-//		eventTitle = new JLabel(e.getEventName());
-//		eventDate = new JLabel(e.getEventDate());
-//		eventTime = new JLabel(e.getEventTime());
-//		eventLocation = new JLabel(e.getEventLoc());
-//		eventDetail = new JLabel(e.getEventDesc());
-//		
-//		messageBoardPanel = new JPanel();
-//		messageBoardLabel = new JLabel("Message Board");
-//		messageBoardPanel.setLayout(new BorderLayout());
-//		messageBoard = new JTextArea();
-//		addPanel = new JPanel();
-//		addPanel.setLayout(new BorderLayout());
-//		addMessageArea = new JTextArea();
-//		sendMessage = new WHButton("Send Message");
-//	}
-//	
-//	private void createGUI() {
-//		mainPanel.setBackground(redColor);
-//		
-//		eventTitle.setFont(font);
-//		font = font.deriveFont(20);
-//		eventDate.setFont(font);
-//		eventTime.setFont(font);
-//		font = font.deriveFont(18);
-//		eventLocation.setFont(font);
-//		font = font.deriveFont(13);
-//		eventDetail.setFont(font);
-//		
-//		GridBagConstraints gbc = new GridBagConstraints();
-//		gbc.anchor = GridBagConstraints.PAGE_START;
-//		add(eventTitle, gbc);
-//		
-//		eventInfoPanel.add(eventDate);
-//		eventInfoPanel.add(Box.createVerticalGlue());
-//		eventInfoPanel.add(eventTime);
-//		eventInfoPanel.add(Box.createVerticalGlue());
-//		eventInfoPanel.add(eventLocation);
-//		eventInfoPanel.add(Box.createVerticalGlue());
-//		eventInfoPanel.add(eventDetail);
-//		gbc.anchor = GridBagConstraints.LINE_START;
-//		gbc.gridx = 500;
-//		gbc.gridy = 200;
-//		eventInfoPanel.setBackground(redColor);
-//		add(eventInfoPanel, gbc);
-//		
-//		messageBoardPanel.setSize(100, 300);
-//		font = font.deriveFont(16);
-//		messageBoardLabel.setFont(font);
-//		messageBoardPanel.add(messageBoardLabel, BorderLayout.NORTH);
-//		updateMessageBoard();
-//		messageBoardPanel.add(messageBoard, BorderLayout.CENTER);
-//		addPanel.setSize(100, 100);
-//		addPanel.add(addMessageArea, BorderLayout.CENTER);
-//		addPanel.add(sendMessage, BorderLayout.SOUTH);
-//		addPanel.setBackground(redColor);
-//		messageBoardPanel.add(addPanel, BorderLayout.SOUTH);
-//		gbc.anchor = GridBagConstraints.LINE_END;
-//		gbc.gridx = 300;
-//		gbc.gridy = 500;
-//		messageBoardPanel.setBackground(redColor);
-//		add(messageBoardPanel, gbc);
-//		
-//		setVisible(true);
-//		
-//	}
-//	
-//	private void addActions() {
-//		
-//	}
-//	
-//	// called when new messages are sent to the server so that message board is updated.
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.Vector;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import Model.Event;
+import customui.WHButton;
+
+public class EventDetailGUI extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	private Event e;
+	private Color yellowColor;
+	private Color highlightColor;
+	private JPanel mainPanel;
+	
+	private WHButton backButton;
+	private JLabel eventTitle;
+	private JPanel eventInfoPanel;
+	private JLabel eventDate;
+	private JLabel eventStartTime;
+	private JLabel eventEndTime;
+	private JLabel eventLocation;
+	private JLabel eventDetail;
+	private JLabel attending;
+	
+	private JPanel messageBoardPanel;
+	private JPanel conversationPanel;
+	private JLabel messageBoardLabel;
+	private JTextArea messageBoard;
+	private JScrollPane messageBoardPane;
+	private JPanel addPanel;
+	private JTextArea addMessageArea;
+	private WHButton postMessage;
+	private JPanel westPanel;
+	private JPanel centerPanel;
+	private JPanel centerNorthPanel;
+	private JPanel centerCenterPanel;
+	
+	public EventDetailGUI(Event e) {
+		super(new BorderLayout());
+		this.setSize(new Dimension(900,602));
+		this.e = e;
+		
+		instantiateComponents();
+		createGUI();
+		addActions();
+	}
+	
+	private void instantiateComponents() {
+		yellowColor = new Color(255,204,102);
+		highlightColor = new Color(247,234,161);
+		mainPanel = new JPanel();
+		//mainPanel.setBackground(yellowColor);
+		
+		
+		
+		backButton = new WHButton("< Back");
+		eventInfoPanel = new JPanel();
+		eventInfoPanel.setLayout(new BoxLayout(eventInfoPanel, BoxLayout.Y_AXIS));
+		eventTitle = new JLabel(e.getEventName());
+		eventDate = new JLabel(e.getEventDate());
+		eventStartTime = new JLabel(e.getEventStartTime());
+		eventEndTime = new JLabel(e.getEventEndTime());
+		eventLocation = new JLabel(e.getEventLoc());
+		eventDetail = new JLabel(e.getEventDesc());
+		
+		messageBoardPanel = new JPanel();
+		messageBoardPanel.setBackground(yellowColor);
+		conversationPanel = new JPanel();
+		conversationPanel.setBackground(yellowColor);
+		messageBoardLabel = new JLabel("Message Board");
+		messageBoardPanel.setLayout(new BorderLayout());
+		messageBoardPanel.setBackground(yellowColor);
+		messageBoard = new JTextArea();
+		messageBoardPane = new JScrollPane(messageBoard);
+		messageBoard.setPreferredSize(new Dimension (292,406));
+		messageBoard.setEditable(false);
+		messageBoardPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		addPanel = new JPanel();
+		addPanel.setLayout(new BorderLayout());
+		addPanel.setBackground(yellowColor);
+		addMessageArea = new JTextArea(5, 1);
+		addMessageArea.setEditable(true);
+		addMessageArea.setLineWrap(true);
+		addMessageArea.setWrapStyleWord(true);
+		addMessageArea.setSelectionColor( highlightColor);
+		
+		postMessage = new WHButton("Post Message");
+		
+		westPanel = new JPanel();
+		westPanel.setBackground(yellowColor);
+		
+		centerPanel = new JPanel();
+		//centerPanel.setBackground(yellowColor);
+		centerNorthPanel = new JPanel();
+		centerNorthPanel.setBackground(yellowColor);
+		centerCenterPanel = new JPanel();
+		
+		attending = new JLabel( e.getAttendees() + " attending");
+		
+
+		
+	}
+	
+	private void createGUI() {
+		
+	
+
+		westPanel.add(backButton, BorderLayout.NORTH);
+		add(westPanel, BorderLayout.WEST);
+		
+//		updateMessageBoard();		
+		Dimension mbSize = new Dimension (300,600);
+		messageBoardPanel.setPreferredSize(mbSize);
+		messageBoardLabel.setFont(new Font ("Phosphate", Font.PLAIN, 30));
+		messageBoardPanel.add(messageBoardLabel, BorderLayout.NORTH);
+		conversationPanel.add(messageBoard, BorderLayout.NORTH);
+		conversationPanel.add(addMessageArea, BorderLayout.SOUTH);
+		messageBoardPanel.add(conversationPanel, BorderLayout.CENTER);
+		addPanel.setSize(100, 100);
+		addPanel.add(addMessageArea, BorderLayout.CENTER);
+		addPanel.add(postMessage, BorderLayout.SOUTH);
+		addPanel.setBackground(yellowColor);
+		messageBoardPanel.add(addPanel, BorderLayout.SOUTH);
+		messageBoardPanel.setBackground(yellowColor);
+		add(messageBoardPanel, BorderLayout.EAST);
+		
+		
+		eventTitle.setFont(new Font ("Phosphate", Font.BOLD, 45));
+		attending.setFont(new Font ("Impact", Font.PLAIN, 25));
+		
+		JPanel titlePanel = new JPanel();
+		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
+		titlePanel.add(eventTitle);
+		titlePanel.add(Box.createVerticalGlue());
+
+		titlePanel.add(Box.createRigidArea(new Dimension(25,25)));
+		JLabel votes = new JLabel("" + e.getUpvotes());
+		votes.setFont(new Font ("Impact", Font.PLAIN, 25));
+		
+		JPanel upvotes = new JPanel();
+		upvotes.add(votes);
+		titlePanel.add(attending);
+		titlePanel.add(Box.createHorizontalGlue());
+		titlePanel.add(upvotes);
+		
+//		centerPanel.add(attending);
+		
+		mainPanel.add(centerPanel, BorderLayout.SOUTH);
+		mainPanel.add(titlePanel,BorderLayout.NORTH);
+	
+		
+		add(mainPanel, BorderLayout.CENTER);
+		
+		setVisible(true);
+		
+		
+	}
+	
+	private void addActions() {
+		
+	}
+	
+	// called when new messages are sent to the server so that message board is updated.
 //	private void updateMessageBoard() {
 //		Vector<Message> messageBoardContents = e.getMessageBoard();
 //		for (int i=0; i<messageBoardContents.size(); i++) {
@@ -136,12 +185,13 @@ package client;
 //			messageBoard.append(m.username()+": "+m.message()+"\n");
 //		}
 //	}
-//	
-//	public static void main (String [] args) {
-//		JFrame f = new JFrame();
-//		f.setSize(new Dimension(900,602));
-//		Event e = new Event("Game", "April 10, 2016", "12:00", "Baseball game, USC v. UCLA", "Dedeaux Field", true, false, false, false);
-//		f.add(new EventDetailGUI(e), BorderLayout.CENTER);
-//		f.setVisible(true);
-//	}
-//}
+	
+	public static void main (String [] args) {
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(new Dimension(900,602));
+		Event e = new Event("BaseBall Game", "April 10, 2016", "12:00 p.m.", "4:00 p.m.", "USC Trojans Baseball will be playing against the UCLA Bruins! ", "Dedeaux Field", 1 , "10:00 a.m.");
+		f.add(new EventDetailGUI(e), BorderLayout.CENTER);
+		f.setVisible(true);
+	}
+}
